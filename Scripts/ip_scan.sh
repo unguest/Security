8b59e7290f084e1a399f4d2bdbcc79a441b33bca
unguest <unguestdev@gmail.com>
2020-06-26 14:28:25 +0200

    Added a waiting function to the ip scanner

--- Scripts/ip_scan.sh
+++ Scripts/ip_scan.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$1" == ""]
+if [[ "$1" == "" ]]
 then
-echo "Usage : " + $0 + " <ip>" # Usage message when no IP is provided
+echo "Usage :" $0 "<ip>" # Usage message when no IP is provided
 
 else # If an IP is specified
 
 for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | cut -d ":" -f 1 &
+pids[${i}]=$!
+done
+
+# Wait for all pids to finish their jobs
+for pid in ${pids[*]}; do
+    wait $pid
 done
 
 fi # End of if
+
+echo "Script terminated."
