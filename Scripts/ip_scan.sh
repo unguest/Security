a2e5e4713ead2174f7ad1a13d021412b9bbc3ad8
unguest <unguestdev@gmail.com>
2020-07-30 19:42:22 +0200

    Update

--- Scripts/ip_scan.sh
+++ Scripts/ip_scan.sh
@@ -6,10 +6,8 @@ echo "Usage :" $0 "<ip>" # Usage message when no IP is provided
 
 else # If an IP is specified
 
-declare -A ips
-
 for ip in `seq 1 254`; do
-ips[${ip}] = ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | cut -d ":" -f 1 &
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | cut -d ":" -f 1 &
 pids[${i}]=$! # Store all pids in an array
 done
 
@@ -20,4 +18,4 @@ done
 
 fi # End of if
 
-echo (echo ${ips[@]}) | sort sort -n -t . -k 4
+echo "Script terminated."
